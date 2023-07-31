6a9960079caf48fba9458d42393605b1eab154ff
raditya2002 <sayaiok2002@gmail.com>
2023-07-31 10:02:00 +0700

    tes

---
+++ app/Http/Controllers/SuperAdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SuperAdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('role:ROLE_SUPERADMIN');
+    }
+
+    /**
+     * Summary of index
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        return view('superadmin.home');
+    }
+}
